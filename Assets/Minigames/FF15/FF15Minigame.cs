0a32ced9a5ad7329ac18c8b741d70374a82a87e5
Ethan Toney <egto232@gmail.com>
2024-05-19 11:44:52 -0400

    ff15 polish

--- Assets/Minigames/FF15/FF15Minigame.cs
+++ Assets/Minigames/FF15/FF15Minigame.cs
@@ -9,7 +9,9 @@ public class FF15Minigame : Minigame
 	public UIDocument fishingUI;
 	public Transform fish;
 	public float catchSpeed = 1;
-	public float healthLossSpeed = 125;
+	public float healthLossSpeed = 100;
+	public float healthGainSpeed = 0;
+	public float healthInputPenalty = 15;
 	public float catchPositionSuccess = -15;
 	private VisualElement healthBar;
 	public GameObject button;
@@ -24,7 +26,8 @@ public class FF15Minigame : Minigame
 	private float nextInputTimer = 1;
 	private char nextInput = ' ';
 	private bool waitingForInput = false;
-	private float health = 250;
+	private float health = 300;
+	private float colorTimer = 0;
 
 	void Start()
     {
@@ -37,6 +40,11 @@ public class FF15Minigame : Minigame
 	{
 		//// check if time to prompt user for input
 		nextInputTimer -= Time.deltaTime;
+		colorTimer -= Time.deltaTime;
+
+		if (colorTimer <= 0) {
+			button.GetComponent<SpriteRenderer>().color = Color.white;
+		}
 
 		if (waitingForInput == false && nextInputTimer <= 0) {
 			int nextInputIndex = Random.Range(0, inputOptions.Length);
@@ -61,25 +69,42 @@ public class FF15Minigame : Minigame
 		}
 
 		if (waitingForInput == true) {
-			bool doneWaiting = false;
+			bool inputSuccess = false;
+			bool inputFailure = false;
+
+			bool inputW = Input.GetKeyDown(KeyCode.W);
+			bool inputS = Input.GetKeyDown(KeyCode.S);
+			bool inputA = Input.GetKeyDown(KeyCode.A);
+			bool inputD = Input.GetKeyDown(KeyCode.D);
+
 			switch (nextInput) {
 				case 'w':
-					doneWaiting = Input.GetKey(KeyCode.W) == true;
+					inputSuccess = inputW;
+					inputFailure = inputS || inputA || inputD;
 					break;
 				case 's':
-					doneWaiting = Input.GetKey(KeyCode.S) == true;
+					inputSuccess = inputS;
+					inputFailure = inputW || inputA || inputD;
 					break;
 				case 'a':
-					doneWaiting = Input.GetKey(KeyCode.A) == true;
+					inputSuccess = inputA;
+					inputFailure = inputW || inputS || inputD;
 					break;
 				case 'd':
-					doneWaiting = Input.GetKey(KeyCode.D) == true;
+					inputSuccess = inputD;
+					inputFailure = inputW || inputS || inputA;
 					break;
 			}
 
-			if (doneWaiting) {
+			if (inputFailure) {
+				button.GetComponent<SpriteRenderer>().color = Color.red;
+				colorTimer = .25f;
+				health -= healthInputPenalty;
+
+			} else if (inputSuccess) {
 				nextInputTimer = Random.Range(.5f, 1.5f);
 				waitingForInput = false;
+
 				button.SetActive(false);
 				letterW.SetActive(false);
 				letterS.SetActive(false);
@@ -91,10 +116,12 @@ public class FF15Minigame : Minigame
 		//// update health
 		if (waitingForInput == true) {
 			health -= healthLossSpeed * Time.deltaTime;
+		} else {
+			health += healthGainSpeed * Time.deltaTime;
 		}
 
 		// clip health
-		health = Mathf.Clamp(health, 0, 250);
+		health = Mathf.Clamp(health, 0, 300);
 
 		//// fish is always moving toward us
 		if (waitingForInput == false) {
